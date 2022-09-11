a991d7dae6a079a06a2b06b8cda1d3b9987b5b5a
GeekchanskiY <astrovskiy.04@gmail.com>
2022-09-11 16:04:25 +0300

    serializers created

--- ymarket/main/serializers.py
+++ ymarket/main/serializers.py
@@ -1,3 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import OfferCategory, Offer, Params
 
+
+class OfferCategorySerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = OfferCategory
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class OfferSerializer(HyperlinkedModelSerializer):
+    category = OfferCategorySerializer
+
+    class Meta:
+        model = Offer
+        fields = ('id', 'name', 'picture', 'description', 'brand', 'category', 'bar_code', 'params', 'price',
+                  'currency', 'vat', 'home_url', 'min_quantity', 'step_quantity', 'dimensions', 'weight', 'disabled',
+                  'amount')
+        read_only_fields = ('id',)
