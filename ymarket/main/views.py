a86d1922db1bea5708dfb868a4f170179de3e8dd
GeekchanskiY <astrovskiy.04@gmail.com>
2022-09-11 15:51:43 +0300

    test view created

--- ymarket/main/views.py
+++ ymarket/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+
+class Test(APIView):
+    def get(self, requests, format=None):
+        return Response("AAA", status=status.HTTP_200_OK)
+
+
