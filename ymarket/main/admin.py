115f4c788265ae4b849d5e98d300124203f573c3
GeekchanskiY <astrovskiy.04@gmail.com>
2022-09-11 16:10:44 +0300

    admin created

--- ymarket/main/admin.py
+++ ymarket/main/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Offer, OfferCategory, Params
 # Register your models here.
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'amount')
+    search_fields = ('name',)
+    fields = ('name', 'picture', 'description', 'brand', 'category', 'bar_code', 'params', 'price',
+              'currency', 'vat', 'home_url', 'min_quantity', 'step_quantity', 'dimensions', 'weight', 'disabled',
+              'amount')
+
+
+@admin.register(OfferCategory)
+class OfferCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    fields = ('name',)
+
+
+@admin.register(Params)
+class ParamsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'value', 'category')
+    search_fields = ('name', )
+    fields = ('name', 'value', 'category')
+
