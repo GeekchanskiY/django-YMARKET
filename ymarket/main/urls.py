2c72eb243458687c277e7e8fa10b6296423685ce
GeekchanskiY <astrovskiy.04@gmail.com>
2022-09-13 02:57:34 +0300

    Update viewset added

--- ymarket/main/urls.py
+++ ymarket/main/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import Test
+from .views import Import, Update
 
 urlpatterns = [
-    path('', Test.as_view(), name='test')
+    path('', Import.as_view(), name='test'),
+    path('upd/', Update.as_view(), name='update')
 ]
