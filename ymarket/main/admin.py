15394049b7b47f2ae0f6adba762abc19b6799546
GeekchanskiY <astrovskiy.04@gmail.com>
2022-09-14 19:08:11 +0300

    filter added

--- ymarket/main/admin.py
+++ ymarket/main/admin.py
@@ -7,6 +7,8 @@ from .models import Offer, OfferCategory, Params
 class OfferAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'amount')
     search_fields = ('name',)
+    list_filter = ('modified', 'disabled')
+    
     fields = ('name', 'picture', 'description', 'brand', 'category', 'bar_code', 'params', 'price',
               'currency', 'vat', 'home_url', 'min_quantity', 'step_quantity', 'dimensions', 'weight', 'disabled',
               'amount', 'modified', 'created')
